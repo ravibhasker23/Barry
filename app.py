128cceb15e18b9cf391dde3b3e2e609527d25071
ravibhasker23 <ravibhasker23@gmail.com>
2017-03-23 17:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def makeWebhookResult(req):
     print(speech)
 
     return {
-        "speech": speech,
+        "speech": "ABC",
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
